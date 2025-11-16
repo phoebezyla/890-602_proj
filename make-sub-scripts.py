4a1bfa2be64efaef35eb74badc15c0276fae421c
phoebezyla <137206218+phoebezyla@users.noreply.github.com>
2025-11-16 14:42:39 -0500

    Update make-sub-scripts.py

--- make-sub-scripts.py
+++ make-sub-scripts.py
@@ -1,3 +1,9 @@
+'''
+A script to create job submission scripts for `aerie-apps-make-hawc-maps`
+
+Takes input .txt files (which list .xcd files for each chunk) and creates one ,fits.gz file for each bin for each chunk. Cuts file, parameter file, and input .txt files must be changed depending on the working bin group, and whether it uses DI or randBkg for its background estimator. 
+
+'''
 import glob
 import os
 import sys
@@ -5,20 +11,23 @@ from math import ceil
 
 outputDir = "/lustre/hawcz01/scratch/userspace/zylaphoe/maps/raw_100"
 aerie = "source /data/disk01/home/zylaphoe/hawc_software/init_aerie.sh"
-i=1
 
-#cuts="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_0-1.txt" #Bins 0-1, DI 
-#cuts="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_2-5.txt" #Bins 2-5, DI
-cuts="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_6-7.txt" #Bins 6-7, DI
-#cuts="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_7up_noDI.txt" #Bins 7-10, noDI 
+chunkStart = 1 #first chunk to include
+chunkEnd = 1511 #last chunk to include + 1 
+groupNames = ['01','25','67','710']
+
+cuts_01="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_0-1.txt" #Bins 0-1, DI 
+cuts_25="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_2-5.txt" #Bins 2-5, DI
+cuts_67="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_6-7.txt" #Bins 6-7, DI
+cuts_710="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_7up_noDI.txt" #Bins 7-10, noDI 
+cuts_arr = [cuts_01, cuts_25, cuts_67, cuts_710]
 
 paramsDI = '-z $CONFIG_HAWC/reconstruction/alignment/align_pass5_v1.0/zenith-pass5-2021-08-13.xml --dtMin 1.2 --dtMax_hr 2.0 --nSide 1024 --roi --useJ2000 --rndSmear'
 paramsNoDI = '-z $CONFIG_HAWC/reconstruction/alignment/align_pass5_v1.0/zenith-pass5-2021-08-13.xml --dtMin 0 --dtMax_hr 2.0 --nSide 1024 --useJ2000 --rndSmear'
+param_arr = [paramsDI, paramsDI, paramsDI, paramsNoDI]
 
-#EDIT THE NEXT TWO LINES WITH THE CHUNKS YOU WANT TO USE TO MAKE MAPS
-
-
-for x in range (1,1511):
+for x in range (chunkStart,chunkEnd):
+    for i in range(4):
     #path = "`envsubst < /lustre/hawcz01/scratch/userspace/dezhih/service/nn_estimator/nn_sky_map/chunk_map/pass5-chunk-1000-1090-ML/pass5_allbin/chunk%06d.txt`"%(x) #Bins 0-1, DI
     #path = "`envsubst < /lustre/hawcz01/scratch/userspace/dezhih/service/nn_estimator/nn_sky_map/chunk_map/pass5-chunk-1000-1090-ML/pass5_bin2up/chunk%06d.txt`"%(x) #Bins 2-5, DI
     path = "`envsubst < /lustre/hawcz01/scratch/userspace/dezhih/service/nn_estimator/nn_sky_map/chunk_map/pass5-chunk-1000-1090-ML/pass5_bin6up/chunk%06d.txt`"%(x) #Bins 6-10, DI and noDi
@@ -41,23 +50,4 @@ for x in range (1,1511):
     print >>outfile, "which offline-reconstructor" 
     print >>outfile, "aerie-apps-make-hawc-maps --input %s -p %s -n ch%04d --cutFile %s %s"%(path,outputDir,x,cuts,paramsNoDI)
     outfile.close()
-
-    #for y in range (1, 2):
-    #    outfile = open("ch%04d-nonDI-%d-pass5-fHit-2pct-ML.sh"%(x,y), "w")
-    #    i=i+1
-    #    print >>outfile,"""#!/bin/sh
-#SBATCH --time=168:00:00 --mem-per-cpu=8000mb
-#
-# INDIR:
-# __INDIR__
-# 
-# RUN:
-# __RUN__
-
-#"""
-    #    print >>outfile, "%s"%(aerie)
-    #    print >>outfile, "which offline-reconstructor"
-    #    print >>outfile, "aerie-apps-make-hawc-maps --input %s -p %s -n ch%04d --cutFile %s/cuts-nonDI-%d.txt %s"%(path,outputDir,x,cutsDir,y,paramsNoDI)
-
-    #    outfile.close()
 
