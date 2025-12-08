83b67d2a4636fb36a7221e23437b2f0dd7f15caf
phoebezyla <137206218+phoebezyla@users.noreply.github.com>
2025-12-08 17:04:57 -0500

    Update make-sub-scripts.py
    Add stronger variable comments

--- make-sub-scripts.py
+++ make-sub-scripts.py
@@ -10,20 +10,24 @@ import sys
 from math import ceil
 
 outputDir = "/lustre/hawcz01/scratch/userspace/zylaphoe/maps/raw_100"
+    # choose where you want raw .fits.gz files to be output
 aerie = "source /data/disk01/home/zylaphoe/hawc_software/init_aerie.sh"
+    # can source my aerie version, or your own! see aerie documentation on HAWC wiki to install
 
 chunkStart = 1 #first chunk to include
 chunkEnd = 1511 #last chunk to include + 1 
-groupNames = ['B0-1','B2-5','B6-7','B7-10-noDI']
+groupNames = ['B0-1','B2-5','B6-7','B7-10-noDI'] # bin group names -- to be appended to output file name
 
 cuts_01="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_0-1.txt" #Bins 0-1, DI 
 cuts_25="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_2-5.txt" #Bins 2-5, DI
 cuts_67="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_6-7.txt" #Bins 6-7, DI
 cuts_710="/data/disk01/home/zylaphoe/map-making-ML/fNhit_100pct_pgamma_mlp_cut_7up_noDI.txt" #Bins 7-10, noDI 
+    # cut files also present in git repo -- rename these to where you copy in your directory
 cuts_arr = [cuts_01, cuts_25, cuts_67, cuts_710]
 
 paramsDI = '-z $CONFIG_HAWC/reconstruction/alignment/align_pass5_v1.0/zenith-pass5-2021-08-13.xml --dtMin 1.2 --dtMax_hr 2.0 --nSide 1024 --roi --useJ2000 --rndSmear'
 paramsNoDI = '-z $CONFIG_HAWC/reconstruction/alignment/align_pass5_v1.0/zenith-pass5-2021-08-13.xml --dtMin 0 --dtMax_hr 2.0 --nSide 1024 --useJ2000 --rndSmear'
+    # parameter files should be in personal $CONFIG_HAWC if aerie installed and set up correctly
 param_arr = [paramsDI, paramsDI, paramsDI, paramsNoDI]
 
 for x in range (chunkStart,chunkEnd): # iterate over chunks
@@ -35,7 +39,8 @@ for x in range (chunkStart,chunkEnd): # iterate over chunks
             path = "`envsubst < /lustre/hawcz01/scratch/userspace/dezhih/service/nn_estimator/nn_sky_map/chunk_map/pass5-chunk-1000-1090-ML/pass5_bin2up/chunk%06d.txt`"%(x) #Bins 2-5, DI
         else:
             path = "`envsubst < /lustre/hawcz01/scratch/userspace/dezhih/service/nn_estimator/nn_sky_map/chunk_map/pass5-chunk-1000-1090-ML/pass5_bin6up/chunk%06d.txt`"%(x) #Bins 6-10, DI and noDI
-
+        # need to be running on HAWC cluster for these to work. these .txt files point to data files too large to copy elsewhere
+        
         file_name = "ch%04d-%s.sh"%(x,groupNames[i])
 
         outfile = open(file_name, "w") #CHANGE W CUT FILE
